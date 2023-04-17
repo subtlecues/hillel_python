2caf6102df091f7f23b04036bd68ed806369f875
Dmytro Vaskovets <dmitriy.vaskovets@gmail.com>
2023-04-18 00:33:49 +0800

    mongo-docker-hostname

--- job_research_crm/mongo_lib.py
+++ job_research_crm/mongo_lib.py
@@ -1,6 +1,6 @@
 import pymongo
 
 class MongoLibrary():
-    client = pymongo.MongoClient('mongodb://root:example@127.0.0.1:27017')
+    client = pymongo.MongoClient('mongodb://root:example@127.0.0.1:27017, mongodb:27017')
     database = client['crm_db']
     contacts_collection = database['contacts']
